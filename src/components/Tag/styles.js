61eed2ab41f592920c04369c9fbe05f7f0de5e26
Your Name <you@example.com>
2023-08-30 13:41:22 -0300

    ajust tags for padding

--- src/components/Tag/styles.js
+++ src/components/Tag/styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 export const Container = styled.span`
   font-size: 12px;
-  padding: 5px 14px;
+  padding: 8px 14px;
   border-radius: 5px;
-  margin-left: 42px;
+  margin-left: 38px;
   color: ${({theme}) => theme.COLORS.WHITE};
   background-color: ${({theme}) => theme.COLORS.ROSEOP};
 `;
